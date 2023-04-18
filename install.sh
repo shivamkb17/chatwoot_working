f06c6a8571ce941c5e4744017f76a27d96f4fba0
Shivam Kumar <csautomaytic@gmail.com>
2023-04-18 18:16:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -331,7 +331,7 @@ function setup_chatwoot() {
   rvm install "ruby-3.1.3"
   rvm use 3.1.3 --default
 
-  git clone https://github.com/voko3456/chatwoot_repo_voko.git
+  git clone https://github.com/shivamkb17/chatwoot_working.git chatwoot
   cd chatwoot
   git checkout "$BRANCH"
   bundle
